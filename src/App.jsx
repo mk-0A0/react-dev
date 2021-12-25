db148994425c92dde5980f1cbcf19eb87e547ec5
Momoka Kamijo <85173655+mk-0A0@users.noreply.github.com>
2021-12-25 22:11:20 +0900

    4-4, 4-5

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { ColorMessage } from "./components/ColorMessage";
+
 export const App = () => {
+  const onClickButton = () => {
+    alert();
+  };
+
   return (
     <>
-      <h1>こんにちは</h1>
-      <p>お元気ですか</p>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <ColorMessage />
+      <ColorMessage color="blue">お元気ですか</ColorMessage>
+      <ColorMessage color="pink">元気です</ColorMessage>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
---
+++ src/components/ColorMessage.jsx
@@ -0,0 +1,10 @@
+export const ColorMessage = (props) => {
+  const { color, children } = props;
+
+  const contentStyle = {
+    color,
+    fontSize: "20px"
+  };
+
+  return <p style={contentStyle}>{children}</p>;
+};
